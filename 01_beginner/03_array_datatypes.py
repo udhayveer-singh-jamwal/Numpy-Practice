1c0d31e79eff5bf20e3156fc23a7f86f7289c865
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-01 22:42:23 +0530

    Add numpy array examples and type conversions

--- 01_beginner/03_array_datatypes.py
+++ 01_beginner/03_array_datatypes.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+arr = np.array([1, 2, 3, 4])
+arr_float = np.array([1, 2, 3], dtype=float)
+
+print(arr.dtype)
+print(arr_float.dtype)
+new_arr = arr.astype(float)
+print(new_arr)
