5e3b467149a8b3130a5cf7d677bf9f2f06376a1d
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-03 22:15:01 +0530

    Update 01_array_operations.py

--- 02_intermediate/01_array_operations.py
+++ 02_intermediate/01_array_operations.py
@@ -1,1 +1,18 @@
+import numpy as np
 
+arr1 = np.array([10, 20, 30, 40])
+arr2 = np.array([1, 2, 3, 4])
+
+add = arr1 + arr2
+sub = arr1 - arr2
+mul = arr1 * arr2
+div = arr1 / arr2
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
+
+# Combined operation
+result = (arr1 * 2) + (arr2 ** 2)
+print("Final Result:", result)
