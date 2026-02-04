1682375f71a031bfcb400943b7a3e286bb2085b9
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-04 22:57:33 +0530

    Update 03_boolean_indexing.py

--- 02_intermediate/03_boolean_indexing.py
+++ 02_intermediate/03_boolean_indexing.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+marks = np.array([45, 78, 88, 32, 90, 67, 40])
+passed = marks[marks >= 50]
+failed = marks[marks < 50]
+print("All Marks:", marks)
+print("Passed Students:", passed)
+print("Failed Students:", failed)
+marks[marks < 50] = 50
+print("Updated Marks:", marks)
