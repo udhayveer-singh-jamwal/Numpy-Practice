1137703e2846e2bad0f1e50ff5c6cd60a4aeaee8
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-07 22:59:34 +0530

    Add reshaping and flattening examples with NumPy

--- 02_intermediate/09_reshape_flatten.py
+++ 02_intermediate/09_reshape_flatten.py
@@ -1,1 +1,18 @@
+import numpy as np
+
+arr = np.arange(1, 13)
+
+matrix = arr.reshape(3, 4)
+flat1 = matrix.flatten()
+flat2 = matrix.ravel()
+print("Original Array:", arr)
+print("Reshaped Matrix:\n", matrix)
+print("Flatten:", flat1)
+print("Ravel:", flat2)
+
+matrix[0, 0] = 100
+print("\nAfter change")
+print("Matrix:\n", matrix)
+print("Flatten:", flat1)
+print("Ravel:", flat2)
 
