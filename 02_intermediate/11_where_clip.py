2a213043817a2aa01f99dbd699d641bf8dcb35a5
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-08 22:09:42 +0530

    Add clipping to scores after applying pass rule

--- 02_intermediate/11_where_clip.py
+++ 02_intermediate/11_where_clip.py
@@ -4,6 +4,6 @@ scores = np.array([32, 45, 67, 89, 95, 28])
 
 updated = np.where(scores < 40, 40, scores)
 limited = np.clip(updated, 40, 90)
-
 print("Original Scores:", scores)
 print("After Pass Rule:", updated)
+print("After Clipping:", limited)
