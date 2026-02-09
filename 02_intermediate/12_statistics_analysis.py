9915dc57530ec8c0e659ef009dd6c878f5a6c07c
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-09 22:22:52 +0530

    Add statistical analysis functions and outlier detection

--- 02_intermediate/12_statistics_analysis.py
+++ 02_intermediate/12_statistics_analysis.py
@@ -1,1 +1,16 @@
+import numpy as np
 
+data = np.array([12, 15, 20, 22, 25, 30, 30, 35])
+
+mean = np.mean(data)
+median = np.median(data)
+std_dev = np.std(data)
+variance = np.var(data)
+
+print("Data:", data)
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", std_dev)
+print("Variance:", variance)
+outliers = data[data > mean + std_dev]
+print("Outliers:", outliers)
