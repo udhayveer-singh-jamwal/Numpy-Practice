3bd2e7cf4b3f486506a1b47886e9121cc11c017c
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-10 22:34:11 +0530

    Add vectorization example using NumPy

--- 03_advanced/01_vectorization_vs_loops.py
+++ 03_advanced/01_vectorization_vs_loops.py
@@ -1,1 +1,18 @@
+import numpy as np
+import time
 
+data = np.arange(1, 1_000_001)
+
+# Python loop
+start = time.time()
+s = 0
+for i in data:
+    s += i * 2
+print("Loop Sum:", s)
+print("Loop Time:", time.time() - start)
+
+# NumPy vectorization
+start = time.time()
+result = np.sum(data * 2)
+print("Vectorized Sum:", result)
+print("Vectorized Time:", time.time() - start)
