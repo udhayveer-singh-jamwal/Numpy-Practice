f35bc1819954e0fb6434dfdbd64bf0a1c9a0ace0
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-11 22:47:50 +0530

    Update 02_performance_comparison.py

--- 03_advanced/02_performance_comparison.py
+++ 03_advanced/02_performance_comparison.py
@@ -7,7 +7,6 @@ np_array = np.array(py_list)
 start = time.time()
 py_result = [x * 5 for x in py_list]
 print("Python List Time:", time.time() - start)
-
 start = time.time()
 np_result = np_array * 5
 print("NumPy Array Time:", time.time() - start)
