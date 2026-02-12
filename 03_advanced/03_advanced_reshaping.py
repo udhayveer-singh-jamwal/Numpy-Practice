5672c13292dac09491e92346da95c35024dd4955
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-12 22:40:04 +0530

    Update 03_advanced_reshaping.py

--- 03_advanced/03_advanced_reshaping.py
+++ 03_advanced/03_advanced_reshaping.py
@@ -4,6 +4,7 @@ arr = np.arange(1, 13)
 matrix = arr.reshape(3, 4)
 
 print("Original:\n", matrix)
-
 transposed = matrix.T
+swapped = np.swapaxes(matrix, 0, 1)
 print("Transposed:\n", transposed)
+print("Swapped Axes:\n", swapped)
