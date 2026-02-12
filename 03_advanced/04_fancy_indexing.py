23175e2be71a5cab5bf5d2f07bc0ab2ba5968fc6
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-12 22:41:00 +0530

    Update 04_fancy_indexing.py

--- 03_advanced/04_fancy_indexing.py
+++ 03_advanced/04_fancy_indexing.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+indexes = [0, 2, 4]
+selected = arr[indexes]
+
+print("Original:", arr)
+print("Indexes:", indexes)
+print("Selected:", selected)
+
+# Modify via fancy indexing
+arr[[1, 3]] = 100
+print("Modified:", arr)
