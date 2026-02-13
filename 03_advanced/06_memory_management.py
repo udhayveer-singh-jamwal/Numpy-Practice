a67ff02aa65f0d9bf383604feaf255796c4a097e
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-13 22:27:01 +0530

    Update 06_memory_management.py

--- 03_advanced/06_memory_management.py
+++ 03_advanced/06_memory_management.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+a = np.arange(10)
+b = a.view()
+c = a.copy()
+
+a[0] = 999
+print("Original:", a)
+print("View:", b)
+print("Copy:", c)
+print("a is b:", a.base is None)
+print("b base:", b.base is a)
