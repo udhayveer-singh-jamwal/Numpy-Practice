b66c60145b8b6e1ae28919897da5022edf7fcf66
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-13 22:25:24 +0530

    Update 05_masking_advanced.py

--- 03_advanced/05_masking_advanced.py
+++ 03_advanced/05_masking_advanced.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 scores = np.array([35, 67, 88, 45, 90, 30, 76])
-
 mask = (scores >= 50) & (scores <= 80)
 filtered = scores[mask]
-
+scores[mask] += 5
 print("Scores:", scores)
 print("Mask:", mask)
 print("Filtered:", filtered)
-
+print("Updated Scores:", scores)
