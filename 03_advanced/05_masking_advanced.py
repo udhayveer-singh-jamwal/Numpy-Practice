381bc620a42eb88ae58c2ee03bc6df06b2f714df
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-13 22:24:31 +0530

    Update 05_masking_advanced.py

--- 03_advanced/05_masking_advanced.py
+++ 03_advanced/05_masking_advanced.py
@@ -1,1 +1,11 @@
+import numpy as np
+
+scores = np.array([35, 67, 88, 45, 90, 30, 76])
+
+mask = (scores >= 50) & (scores <= 80)
+filtered = scores[mask]
+
+print("Scores:", scores)
+print("Mask:", mask)
+print("Filtered:", filtered)
 
