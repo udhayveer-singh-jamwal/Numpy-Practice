cbfee9659f495ac3bdce16046b2d55fe12c5b16c
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-14 22:53:31 +0530

    Update 07_window_operations.py

--- 03_advanced/07_window_operations.py
+++ 03_advanced/07_window_operations.py
@@ -1,1 +1,11 @@
+import numpy as np
 
+data = np.array([10, 20, 30, 40, 50, 60, 70])
+window = 3
+moving_avg = []
+for i in range(len(data) - window + 1):
+    avg = data[i:i+window].mean()
+    moving_avg.append(avg)
+
+print("Data:", data)
+print("Moving Average:", moving_avg)
