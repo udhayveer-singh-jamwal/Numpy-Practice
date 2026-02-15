ec2f9c1464178328eb0bc9225db43c1255230af3
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-15 22:32:27 +0530

    Update 09_matrix_multiplication.py

--- 03_advanced/09_matrix_multiplication.py
+++ 03_advanced/09_matrix_multiplication.py
@@ -1,1 +1,11 @@
+import numpy as np
 
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+elementwise = A * B
+dot_product = np.dot(A, B)
+print("Matrix A:\n", A)
+print("Matrix B:\n", B)
+print("Element-wise:\n", elementwise)
+print("Dot Product:\n", dot_product)
