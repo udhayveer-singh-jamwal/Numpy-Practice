e39cb2cdce97dc060dbd27e54cb98e23cb84f1c4
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-15 22:32:43 +0530

    Update 09_matrix_multiplication.py

--- 03_advanced/09_matrix_multiplication.py
+++ 03_advanced/09_matrix_multiplication.py
@@ -2,10 +2,11 @@ import numpy as np
 
 A = np.array([[1, 2], [3, 4]])
 B = np.array([[5, 6], [7, 8]])
-
 elementwise = A * B
 dot_product = np.dot(A, B)
+matmul = A @ B
 print("Matrix A:\n", A)
 print("Matrix B:\n", B)
 print("Element-wise:\n", elementwise)
 print("Dot Product:\n", dot_product)
+print("Using @ operator:\n", matmul)
