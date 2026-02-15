c09ffe568d3c2f6e5c0bcd05f5c40cb22e3345b5
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-15 22:30:47 +0530

    Update 08_linear_algebra.py

--- 03_advanced/08_linear_algebra.py
+++ 03_advanced/08_linear_algebra.py
@@ -1,1 +1,15 @@
+import numpy as np
 
+matrix = np.array([
+    [4, 2],
+    [3, 1]
+])
+
+det = np.linalg.det(matrix)
+inv = np.linalg.inv(matrix)
+eig_values, eig_vectors = np.linalg.eig(matrix)
+print("Matrix:\n", matrix)
+print("Determinant:", det)
+print("Inverse:\n", inv)
+print("Eigenvalues:", eig_values)
+print("Eigenvectors:\n", eig_vectors)
