f7af68a0c6f0f7d37f262b3c16c8271f734a335d
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-16 22:24:29 +0530

    Update 10_random_simulation.py

--- 03_advanced/10_random_simulation.py
+++ 03_advanced/10_random_simulation.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 np.random.seed(42)
-
 random_data = np.random.randint(50, 100, size=100)
-
 mean = random_data.mean()
 std = random_data.std()
-
+above_80 = random_data[random_data > 80]
+print("Count above 80:", len(above_80))
 print("Random Data Sample:", random_data[:10])
 print("Mean:", mean)
 print("Std Dev:", std)
