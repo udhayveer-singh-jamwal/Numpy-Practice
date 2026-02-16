80bbdb1c106c5b512506c20ec61709cf684da657
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-16 22:23:36 +0530

    Update 10_random_simulation.py

--- 03_advanced/10_random_simulation.py
+++ 03_advanced/10_random_simulation.py
@@ -1,1 +1,12 @@
+import numpy as np
 
+np.random.seed(42)
+
+random_data = np.random.randint(50, 100, size=100)
+
+mean = random_data.mean()
+std = random_data.std()
+
+print("Random Data Sample:", random_data[:10])
+print("Mean:", mean)
+print("Std Dev:", std)
