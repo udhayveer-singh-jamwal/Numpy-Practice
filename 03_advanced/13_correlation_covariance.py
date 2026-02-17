70b548829786981eb44b4ddb1645ed1b8e43c0d2
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-17 22:08:38 +0530

    Update 13_correlation_covariance.py

--- 03_advanced/13_correlation_covariance.py
+++ 03_advanced/13_correlation_covariance.py
@@ -3,5 +3,6 @@ import numpy as np
 x = np.array([10, 20, 30, 40, 50])
 y = np.array([15, 25, 35, 45, 60])
 cov_matrix = np.cov(x, y)
+corr_matrix = np.corrcoef(x, y)
 print("Covariance Matrix:\n", cov_matrix)
-
+print("Correlation Matrix:\n", corr_matrix)
