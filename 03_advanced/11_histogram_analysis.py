db24cdf1a1018e2d51cbc66c8b5716aae390b1f7
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-17 22:05:58 +0530

    Update 11_histogram_analysis.py

--- 03_advanced/11_histogram_analysis.py
+++ 03_advanced/11_histogram_analysis.py
@@ -1,1 +1,10 @@
+import numpy as np
+
+data = np.random.normal(loc=50, scale=10, size=1000)
+hist, bins = np.histogram(data, bins=10)
+print("Histogram counts:", hist)
+print("Bin edges:", bins)
+print("Max Value:", data.max())
+print("Min Value:", data.min())
+print("Mean:", data.mean())
 
