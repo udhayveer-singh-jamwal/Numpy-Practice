84f6592c996f96c383bf9057e1bd967772fd445e
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-19 22:09:35 +0530

    Update 13_outlier_detection.py

--- 03_advanced/13_outlier_detection.py
+++ 03_advanced/13_outlier_detection.py
@@ -4,5 +4,7 @@ data = np.array([10, 12, 14, 15, 16, 100])
 mean = data.mean()
 std = data.std()
 z_scores = (data - mean) / std
+outliers = data[np.abs(z_scores) > 2]
+print("Data:", data)
 print("Z-scores:", z_scores)
-
+print("Outliers:", outliers)
