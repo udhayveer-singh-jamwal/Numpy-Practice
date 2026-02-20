1def5d51c5263980548fca46bb540f706691b152
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2026-02-20 22:01:51 +0530

    Update 15_real_world_practice.py

--- 03_advanced/15_real_world_practice.py
+++ 03_advanced/15_real_world_practice.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+sales = np.array([
+    [1200, 1500, 1800],
+    [1000, 1700, 1600],
+    [2000, 2200, 2100],
+    [900,  1100, 1300]
+])
+
+monthly_total = sales.sum(axis=1)
+product_total = sales.sum(axis=0)
+
+print("Sales Data:\n", sales)
+print("Monthly Total:", monthly_total)
